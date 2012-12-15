7ef717146c6ef810882a290ca19590687312d210
indikasampath2000@gmail.com <indikasampath2000@gmail.com@76741e46-650c-ec8c-0b37-cce4cf115db0>
2012-12-15 07:07:35 +0000

    implemented validation for Promotion

--- notification-api-core/src/main/java/ousl/group4/model/PromotionCampaign.java
+++ notification-api-core/src/main/java/ousl/group4/model/PromotionCampaign.java
@@ -3,12 +3,18 @@ package ousl.group4.model;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class PromotionCampaign {
     private String type;
     private String user;
     private String message;
+    private Boolean schedule;
+    private Date scheduleDate;
+    private Integer hour;
+    private Integer minute;
+    private Integer second;
     private List<MultipartFile> files = new ArrayList<MultipartFile>();
     private MultipartFile spreadsheet;
 
@@ -34,6 +40,46 @@ public class PromotionCampaign {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public Boolean getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(Boolean schedule) {
+        this.schedule = schedule;
+    }
+
+    public Date getScheduleDate() {
+        return scheduleDate;
+    }
+
+    public void setScheduleDate(Date scheduleDate) {
+        this.scheduleDate = scheduleDate;
+    }
+
+    public Integer getHour() {
+        return hour;
+    }
+
+    public void setHour(Integer hour) {
+        this.hour = hour;
+    }
+
+    public Integer getMinute() {
+        return minute;
+    }
+
+    public void setMinute(Integer minute) {
+        this.minute = minute;
+    }
+
+    public Integer getSecond() {
+        return second;
+    }
+
+    public void setSecond(Integer second) {
+        this.second = second;
     }
 
     public List<MultipartFile> getFiles() {
