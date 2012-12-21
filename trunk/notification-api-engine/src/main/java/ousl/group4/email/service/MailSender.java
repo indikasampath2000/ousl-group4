beceab8aff2613fa29e78700ea449bf19f759e64
indikasampath2000@gmail.com <indikasampath2000@gmail.com@76741e46-650c-ec8c-0b37-cce4cf115db0>
2012-12-21 17:19:20 +0000

    code review and some bug fixed

--- trunk/notification-api-engine/src/main/java/ousl/group4/email/service/MailSender.java
+++ trunk/notification-api-engine/src/main/java/ousl/group4/email/service/MailSender.java
@@ -14,24 +14,28 @@ public interface MailSender {
 
     /**
      * @param mailMap Must contains the below key names with relevant values
-     *                sender : the sender email address. cannot be null
-     *                recipients : the receiver(s) email addresses and send type (String[][] array object). first array element: email address. second array element: pass one of constant in MailSendType. cannot be null. refer: MailSendType
-     *                subject : the subject of email. cannot be null
-     *                mailBody : the body of email. cannot be null
-     *                attachments : Attachment(s) if necessary (Optional). must contain absolute path of file(s) (String[] array object).
-     *                inlineImages : Inline image(s) if necessary (Optional). must contain absolute path of image(s) and content id (String[][] array object).
+     *                <p>
+     *                sender : the sender email address. cannot be null<br/>
+     *                recipients : the receiver(s) email addresses and send type (String[][] array object). first array element: email address. second array element: pass one of constant in MailSendType. cannot be null. refer: MailSendType<br/>
+     *                subject : the subject of email. cannot be null<br/>
+     *                mailBody : the body of email. cannot be null<br/>
+     *                attachments : Attachment(s) if necessary (Optional). must contain absolute path of file(s) (String[] array object).<br/>
+     *                inlineImages : Inline image(s) if necessary (Optional). must contain absolute path of image(s) and content id (String[][] array object).</p>
+     * @see ousl.group4.email.model.MailKeyBox
      * @throws ousl.group4.exception.NotificationAPIException
      */
     void send(Map<String, Object> mailMap) throws NotificationAPIException;
 
     /**
      * @param mailMap      Must contains the below key names with relevant values
-     *                     sender : the sender email address. cannot be null
-     *                     recipients : the receiver(s) email addresses and send type (String[][] array object). first array element: email address. second array element: pass one of constant in MailSendType. cannot be null. refer: MailSendType
-     *                     subject : the subject of email. cannot be null
-     *                     attachments : Attachment(s) if necessary (Optional). must contain absolute path of file(s) (String[] array object).
-     *                     inlineImages : Inline image(s) if necessary (Optional). must contain absolute path of image(s) and content id (String[][] array object).
-     *                     other key value pair of velocity template's place holders, cannot be null
+     *                     <p>
+     *                     sender : the sender email address. cannot be null<br/>
+     *                     recipients : the receiver(s) email addresses and send type (String[][] array object). first array element: email address. second array element: pass one of constant in MailSendType. cannot be null. refer: MailSendType<br/>
+     *                     subject : the subject of email. cannot be null<br/>
+     *                     attachments : Attachment(s) if necessary (Optional). must contain absolute path of file(s) (String[] array object).<br/>
+     *                     inlineImages : Inline image(s) if necessary (Optional). must contain absolute path of image(s) and content id (String[][] array object).<br/>
+     *                     other key value pair of velocity template's place holders, cannot be null</p>
+     * @see ousl.group4.email.model.MailKeyBox
      * @param templatePath classpath of template. cannot be null
      * @throws Exception
      */
@@ -39,12 +43,14 @@ public interface MailSender {
 
     /**
      * @param mailMap      Must contains the below key names with relevant values
-     *                     sender : the sender email address. cannot be null
-     *                     recipients : the receiver(s) email addresses and send type (String[][] array object). first array element: email address. second array element: pass one of constant in MailSendType. cannot be null. refer: MailSendType
-     *                     subject : the subject of email. cannot be null
-     *                     mailBody : the body of email. cannot be null
-     *                     attachments : Attachment(s) if necessary (Optional). must contain absolute path of file(s) (String[] array object).
-     *                     inlineImages : Inline image(s) if necessary (Optional). must contain absolute path of image(s) and content id (String[][] array object).
+     *                     <p>
+     *                     sender : the sender email address. cannot be null<br/>
+     *                     recipients : the receiver(s) email addresses and send type (String[][] array object). first array element: email address. second array element: pass one of constant in MailSendType. cannot be null. refer: MailSendType<br/>
+     *                     subject : the subject of email. cannot be null<br/>
+     *                     mailBody : the body of email. cannot be null<br/>
+     *                     attachments : Attachment(s) if necessary (Optional). must contain absolute path of file(s) (String[] array object).<br/>
+     *                     inlineImages : Inline image(s) if necessary (Optional). must contain absolute path of image(s) and content id (String[][] array object).</p>
+     * @see ousl.group4.email.model.MailKeyBox
      * @param mailSchedule contains schedule job data
      * @throws Exception
      */
@@ -52,12 +58,14 @@ public interface MailSender {
 
     /**
      * @param mailMap      Must contains the below key names with relevant values
-     *                     sender : the sender email address. cannot be null
-     *                     recipients : the receiver(s) email addresses and send type (String[][] array object). first array element: email address. second array element: pass one of constant in MailSendType. cannot be null. refer: MailSendType
-     *                     subject : the subject of email. cannot be null
-     *                     attachments : Attachment(s) if necessary (Optional). must contain absolute path of file(s) (String[] array object).
-     *                     inlineImages : Inline image(s) if necessary (Optional). must contain absolute path of image(s) and content id (String[][] array object).
-     *                     other key value pair of velocity template's place holders, cannot be null
+     *                     <p>
+     *                     sender : the sender email address. cannot be null<br/>
+     *                     recipients : the receiver(s) email addresses and send type (String[][] array object). first array element: email address. second array element: pass one of constant in MailSendType. cannot be null. refer: MailSendType<br/>
+     *                     subject : the subject of email. cannot be null<br/>
+     *                     attachments : Attachment(s) if necessary (Optional). must contain absolute path of file(s) (String[] array object).<br/>
+     *                     inlineImages : Inline image(s) if necessary (Optional). must contain absolute path of image(s) and content id (String[][] array object).<br/>
+     *                     other key value pair of velocity template's place holders, cannot be null</p>
+     * @see ousl.group4.email.model.MailKeyBox
      * @param templatePath classpath of template. cannot be null
      * @param mailSchedule contains schedule job data
      * @throws ousl.group4.exception.NotificationAPIException
@@ -72,6 +80,7 @@ public interface MailSender {
     List<Mail> getFinishedMailNotifications();
 
     /**
+     * This method return List object of schedule mail notification
      *
      * @return
      */
